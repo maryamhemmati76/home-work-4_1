56a3c0da47f61edd2b0e835d5594a14548e2e4f9
maryamhemmati76 <maryamhemmati0736@gmail.com>
2024-04-20 11:14:34 +0330

    subtract is added

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ x = int(input("Enter the first number : "))
 y = int(input("Enter the second number : "))
 
 from src.summation import add
+from src.subtract import sub
+
 sum_ = add(x, y)
+sub_ = sub(x, y)
 print(f"{x} + {y} = {sum_}")
+print(f"{x} - {y} = {sub_}")
+
 
 with open("calculaton.txt", "w") as file:
     file.write(f"summation is {sum_}")
+    file.write(f"subtract is {sub_}")

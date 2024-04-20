183d9c385b62dec6850550ec286b374d5eabfec2
maryamhemmati76 <maryamhemmati0736@gmail.com>
2024-04-20 11:27:38 +0330

    divide is added

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ y = int(input("Enter the second number : "))
 from src.summation import add
 from src.subtract import sub
 from src.multiply import multiply
+from src.divide import divide
 
 sum_ = add(x, y)
 sub_ = sub(x, y)
 multiply_ = multiply(x, y)
+div = divide(x, y)
 
 print(f"{x} + {y} = {sum_}")
 print(f"{x} - {y} = {sub_}")
 print(f"{x} * {y} = {multiply_}")
+print(f"{x} / {y} = {div}")
 
 
 with open("calculaton.txt", "w") as file:
     file.write(f"summation is {sum_}")
     file.write(f"subtract is {sub_}")
     file.write(f"subtract is {multiply_}")
+    file.write(f"divide is {div}")
